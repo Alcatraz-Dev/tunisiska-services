8386ff17ae477ba6b006d4dee6562d606f039205
Haythem Dhahri <alcatraz3019@gmail.com>
2026-05-07 00:09:07 +0200

    chore: add expo-router/babel and reorder plugins in babel config

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,10 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-    plugins: ["react-native-reanimated/plugin", "react-native-worklets/plugin"],
+    plugins: [
+      "expo-router/babel",
+      "react-native-worklets/plugin",
+      "react-native-reanimated/plugin",
+    ],
   };
 };
